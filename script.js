758d84baf431bfd4e607d99c06ee13a1cd83f64b
Dmitri Dmitri <dbouilov09@gmail.com>
2020-04-02 19:51:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//const axios = require("axios");
+//const util = require("util");
+
+
+console.log("script is connected")
+
+
+//var tableList = async function() {
+    //return "the username from API (SUM FUNCTION) is "+username;
+  
+   // var y = await axios.get(`/api/tables`);
+  
+   // console.log(y)
+  
+   // return y.data;
+ // };
+
+ // console.log("tablelist variableis "+tableList)
